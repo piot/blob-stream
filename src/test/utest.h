d2c4b2bfa75dc4858a5781057312e04b59168892
Peter Bjorklund <piot@hotmail.com>
2023-04-11 11:08:26 +0200

    fix: remove wrong header

--- src/test/utest.h
+++ src/test/utest.h
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Peter Bjorklund. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
 /*
    This is free and unencumbered software released into the public domain.
 
